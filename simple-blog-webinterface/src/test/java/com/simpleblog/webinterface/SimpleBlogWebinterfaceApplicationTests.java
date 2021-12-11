5cba01df67defad5ce101b73c2b011c54fb81460
clembnl <cl.bonal@free.fr>
2021-12-12 00:42:51 +0100

    Ajout partie interface web

---
+++ simple-blog-webinterface/src/test/java/com/simpleblog/webinterface/SimpleBlogWebinterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpleblog.webinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBlogWebinterfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
